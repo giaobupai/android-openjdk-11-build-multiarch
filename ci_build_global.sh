1747decae5a3e734fe6d8b0fe696a8eccd7b1c59
Tungstend <hanjinlong197@gmail.com>
2024-01-16 21:05:09 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -4,20 +4,24 @@ set -e
 
 export JDK_DEBUG_LEVEL=release
 
-if [ "$BUILD_IOS" != "1" ]; then
-  sudo apt update
-  sudo apt -y install autoconf python unzip zip
+sudo apt update
+sudo apt -y install autoconf python unzip zip
 
-  wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
-  ./extractndk.sh
-  ./maketoolchain.sh
-else
-  chmod +x ios-arm64-clang
-  chmod +x ios-arm64-clang++
-  chmod +x macos-host-cc
-fi
+sudo chmod 777 extractndk.sh
+sudo chmod 777 maketoolchain.sh
+
+wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
+./extractndk.sh
+./maketoolchain.sh
 
 # Some modifies to NDK to fix
+
+sudo chmod 777 getlibs.sh
+sudo chmod 777 buildlibs.sh
+sudo chmod 777 clonejdk.sh
+sudo chmod 777 buildjdk.sh
+sudo chmod 777 removejdkdebuginfo.sh
+sudo chmod 777 tarjdk.sh
 
 ./getlibs.sh
 ./buildlibs.sh
