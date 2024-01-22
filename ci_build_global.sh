ef3dd6250861e8a093f71a94604b77ac69ad1fb8
Tungstend <hanjinlong197@gmail.com>
2024-01-22 22:17:31 +0800

    Update ci_build_global.sh

--- ci_build_global.sh
+++ ci_build_global.sh
@@ -7,6 +7,9 @@ export JDK_DEBUG_LEVEL=release
 sudo apt update
 sudo apt -y install autoconf python unzip zip
 
+sudo chmod 777 extractndk.sh
+sudo chmod 777 maketoolchain.sh
+
 wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com/android/repository/android-ndk-$NDK_VERSION-linux-x86_64.zip"
 ./extractndk.sh
 ./maketoolchain.sh
@@ -14,6 +17,12 @@ wget -nc -nv -O android-ndk-$NDK_VERSION-linux-x86_64.zip "https://dl.google.com
 # Some modifies to NDK to fix
 
 sudo chmod 777 getbootjdk.sh
+sudo chmod 777 getlibs.sh
+sudo chmod 777 buildlibs.sh
+sudo chmod 777 clonejdk.sh
+sudo chmod 777 buildjdk.sh
+sudo chmod 777 removejdkdebuginfo.sh
+sudo chmod 777 tarjdk.sh
 
 ./getbootjdk.sh
 ./getlibs.sh
