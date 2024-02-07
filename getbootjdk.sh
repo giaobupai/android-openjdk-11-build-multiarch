3f1c93d442b6e0149c45d79f5f5a4928b023be86
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-08 00:47:32 +0800

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -3,5 +3,5 @@ set -e
 . setdevkitpath.sh
 
 # Temp script to get jdk10
-wget https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz
+wget https://cdn.azul.com/zulu/bin/zulu11.70.15-ca-jdk11.0.22-linux_x64.tar.gz
 tar xvf openjdk-10*.tar.gz
