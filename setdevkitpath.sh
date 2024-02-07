5bea362da2522472f5b11196dec0c25789851990
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-08 00:53:54 +0800

    Update setdevkitpath.sh

--- setdevkitpath.sh
+++ setdevkitpath.sh
@@ -2,17 +2,17 @@
 # https://github.com/PojavLauncherTeam/openjdk-multiarch-jdk8u/blob/aarch64-shenandoah-jdk8u272-b10/jdk/src/share/native/sun/java2d/loops/GraphicsPrimitiveMgr.c
 export NDK_VERSION=r21
 
-if [ -z "$BUILD_FREETYPE_VERSION" ]
+if [[ -z "$BUILD_FREETYPE_VERSION" ]]
 then
   export BUILD_FREETYPE_VERSION="2.10.0"
 fi
 
-if [ -z "$JDK_DEBUG_LEVEL" ]
+if [[ -z "$JDK_DEBUG_LEVEL" ]]
 then
   export JDK_DEBUG_LEVEL=release
 fi
 
-if [ "$TARGET_JDK" == "aarch64" ]
+if [[ "$TARGET_JDK" == "aarch64" ]]
 then
   export TARGET_SHORT=arm64
 else
