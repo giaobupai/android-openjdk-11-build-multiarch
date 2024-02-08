f07a5163e77f18720d2c9923a5ccb10e31b9fa98
aaaapai <89151134+aaaapai@users.noreply.github.com>
2024-02-09 01:21:07 +0800

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -4,4 +4,4 @@ set -e
 
 # Temp script to get jdk10
 wget https://cdn.azul.com/zulu/bin/zulu11.70.15-ca-jdk11.0.22-linux_x64.tar.gz
-tar xvf openjdk-10*.tar.gz
+tar xvf zulu11.70.15-ca-jdk11.0.22-linux_x64.tar.gz
