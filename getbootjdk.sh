ebc1f82c892ab6b1333047ba2c96d4e932a4b2b1
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-09 13:49:32 +0800

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -3,6 +3,6 @@ set -e
 . setdevkitpath.sh
 
 # Temp script to get jdk10
-wget https://download.java.net/java/GA/jdk10/10.0.2/19aef61b38124481863b1413dce1855f/13/openjdk-10.0.2_linux-x64_bin.tar.gz
-tar xvf openjdk-10*.tar.gz
+wget https://download.java.net/java/GA/jdk11/9/GPL/openjdk-11.0.2_linux-x64_bin.tar.gz
+tar xvf openjdk-11*.tar.gz
 
