484847926cdfd51246b08bf511027f17b8b141e7
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-09 11:15:16 +0800

    Update getbootjdk.sh

--- getbootjdk.sh
+++ getbootjdk.sh
@@ -3,6 +3,6 @@ set -e
 . setdevkitpath.sh
 
 # Temp script to get jdk10
-wget https://cdn.azul.com/zulu/bin/zulu10.3+5-jdk10.0.2-linux_x64.tar.gz
-tar xvf zulu10*.tar.gz
+wget https://cdn.azul.com/zulu/bin/zulu11.70.15-ca-jdk11.0.22-linux_x64.tar.gz
+tar xvf zulu11*.tar.gz
 
