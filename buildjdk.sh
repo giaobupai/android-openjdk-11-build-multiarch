71e2d64d0fe8de41183724172a0e96d5c0974dce
giaobupai <137373766+giaobupai@users.noreply.github.com>
2024-02-09 12:26:18 +0800

    Update buildjdk.sh

--- buildjdk.sh
+++ buildjdk.sh
@@ -58,7 +58,7 @@ bash ./configure \
     --enable-option-checking=fatal \
     --enable-headless-only=yes \
     --with-jvm-variants=$JVM_VARIANTS \
-    --with-jvm-features=-dtrace,-zero,-vm-structs,-epsilongc,shenandoahgc,zgc \
+    --with-jvm-features=-dtrace,-zero,-vm-structs,-epsilongc \
     --with-cups-include=$CUPS_DIR \
     --with-devkit=$TOOLCHAIN \
     --with-native-debug-symbols=external \
